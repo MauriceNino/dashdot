7b987da81969f6ee68763b2ae64531440d326631
MauriceNino <mauriceprivat98@gmail.com>
2021-10-29 11:22:39 +0200

    add sass; add styled-components; add themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { default as styled, ThemeProvider } from "styled-components";
+import { useColorScheme } from "use-color-scheme";
+import { darkTheme, lightTheme } from "./theme/theme";
+
+const Container = styled.div`
+  overflow: auto;
+  height: 100vh;
+  width: 100vw;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
 
 function App() {
+  const { scheme } = useColorScheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={scheme === "dark" ? darkTheme : lightTheme}>
+      <Container></Container>
+    </ThemeProvider>
   );
 }
 
