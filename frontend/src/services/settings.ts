2845c58b494754aaf8445df39ae7bde0d6e355f4
MauriceNino <mauriceprivat98@gmail.com>
2021-10-30 21:17:27 +0200

    fix settings overriding when reloading

--- frontend/src/services/settings.ts
+++ frontend/src/services/settings.ts
@@ -5,14 +5,14 @@ type Settings = {
   darkMode?: boolean;
 };
 
-export const setSettings = <T extends keyof Settings = keyof Settings>(
+export const setStoreSetting = <T extends keyof Settings = keyof Settings>(
   property: T,
   value: Settings[T]
 ) => {
   store.set(property, value);
 };
 
-export const getSettings = <T extends keyof Settings = keyof Settings>(
+export const getStoreSetting = <T extends keyof Settings = keyof Settings>(
   property: T
 ) => {
   return store.get(property) as Settings[T];
@@ -27,19 +27,19 @@ export const useSetting = <T extends keyof Settings = keyof Settings>(
   initialValue?: Settings[T]
 ): [Settings[T], Dispatch<Settings[T]>] => {
   const [localValue, setLocalValue] = useState<Settings[T]>(
-    () => getSettings(key) ?? initialValue
+    () => getStoreSetting(key) ?? initialValue
   );
 
   const setSetting = (newValue: Settings[T]) => {
     ALL_DISPATCHES[key]!.forEach((dispatch) => dispatch(newValue));
-    setSettings(key, newValue);
+    setStoreSetting(key, newValue);
   };
 
   useEffect(() => {
     ALL_DISPATCHES[key] = ALL_DISPATCHES[key] ?? [];
     ALL_DISPATCHES[key]!.push(setLocalValue);
 
-    if (initialValue !== undefined) {
+    if (initialValue !== undefined && getStoreSetting(key) === undefined) {
       setSetting(initialValue);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
