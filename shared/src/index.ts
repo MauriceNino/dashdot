18e927815fee8c4395c5c1ad92468d9d7b2e2e2f
MauriceNino <mauriceprivat98@gmail.com>
2021-10-30 16:17:37 +0200

    add shared module + global build/dev commands

---
+++ shared/src/index.ts
@@ -0,0 +1,12 @@
+export type CpuInfo = {};
+export type CpuLoad = {};
+
+export type RamInfo = {};
+export type RamLoad = {};
+
+export type DiskInfo = {};
+export type DiskLoad = {};
+
+export type OsInfo = {};
+
+export type HardwareInfo = CpuInfo & RamInfo & DiskInfo & OsInfo;
