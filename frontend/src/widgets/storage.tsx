97cf74b5e0ae5a098b40060a11bde165e14b85d6
MauriceNino <mauriceprivat98@gmail.com>
2021-11-01 18:41:36 +0100

    show pie chart on storage

--- frontend/src/widgets/storage.tsx
+++ frontend/src/widgets/storage.tsx
@@ -1,12 +1,18 @@
 import { faHdd } from "@fortawesome/free-solid-svg-icons";
-import { StorageInfo } from "dashdot-shared";
+import { ResponsivePie } from "@nivo/pie";
+import { StorageInfo, StorageLoad } from "dashdot-shared";
 import { FC } from "react";
 import { useTheme } from "styled-components";
 import HardwareInfoContainer from "../components/hardware-info-container";
+import ThemedText from "../components/text";
 import { removeDuplicates } from "../utils/array-utils";
 import { byteToGb } from "../utils/calculations";
 
-const StorageWidget: FC<Partial<StorageInfo>> = (props) => {
+type StorageWidgetProps = {
+  load?: StorageLoad;
+} & Partial<StorageInfo>;
+
+const StorageWidget: FC<StorageWidgetProps> = (props) => {
   const theme = useTheme();
 
   const size = props.layout?.reduce((acc, cur) => acc + cur.size, 0);
@@ -18,7 +24,7 @@ const StorageWidget: FC<Partial<StorageInfo>> = (props) => {
   return (
     <HardwareInfoContainer
       color={theme.colors.storagePrimary}
-      contentLoaded={false}
+      contentLoaded={props.load != null}
       heading="Storage"
       infos={[
         {
@@ -35,7 +41,43 @@ const StorageWidget: FC<Partial<StorageInfo>> = (props) => {
         },
       ]}
       icon={faHdd}
-    />
+    >
+      <ResponsivePie
+        data={[
+          {
+            id: "Used",
+            value: props.load?.used,
+          },
+          {
+            id: "Free",
+            value: props.load?.free,
+          },
+        ]}
+        margin={{ top: 40, bottom: 40 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={10}
+        activeOuterRadiusOffset={8}
+        arcLinkLabelsTextColor={theme.colors.text}
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: "color" }}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{
+          from: "color",
+          modifiers: [[theme.dark ? "brighter" : "darker", 3]],
+        }}
+        arcLabel={(data) => `${byteToGb(data.value)} GB`}
+        colors={[theme.colors.storagePrimary, theme.colors.background]}
+        tooltip={(props) => {
+          const value = props.datum.value;
+          return (
+            <ThemedText>
+              {props.datum.id}: {byteToGb(value)} GB
+            </ThemedText>
+          );
+        }}
+      />
+    </HardwareInfoContainer>
   );
 };
 
