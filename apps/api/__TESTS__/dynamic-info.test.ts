cdd8f89b39112a535fbf6b13fc3f0b680a28fd4f
MauriceNino <mauriceprivat98@gmail.com>
2022-08-28 12:32:14 +0200

    test(api): add test for #292
    fixes #292

--- apps/api/__TESTS__/dynamic-info.test.ts
+++ apps/api/__TESTS__/dynamic-info.test.ts
@@ -1,18 +1,19 @@
 import { expect } from 'chai';
 import { mapToStorageOutput } from '../src/dynamic-info';
-import { TEST_CASE_1 } from './test-cases';
+import { TEST_CASE_1, TEST_CASE_2 } from './test-cases';
+
+const toStorageInp = (inp: { layout: any; blocks: any; sizes: any }) =>
+  [inp.layout, inp.blocks, inp.sizes] as const;
 
 describe('Dynamic Info', () => {
   describe('Storage', () => {
     it('Test Case 1', () => {
-      const output = mapToStorageOutput(
-        TEST_CASE_1.layout,
-        //@ts-ignore
-        TEST_CASE_1.blocks,
-        TEST_CASE_1.sizes
-      );
-
-      expect(output).to.deep.equal({ layout: [{ load: 185203033088 }] });
+      const output = mapToStorageOutput(...toStorageInp(TEST_CASE_1));
+      expect(output).to.deep.equal(TEST_CASE_1.output);
+    });
+    it('Test Case 2', () => {
+      const output = mapToStorageOutput(...toStorageInp(TEST_CASE_2));
+      expect(output).to.deep.equal(TEST_CASE_2.output);
     });
   });
 });
--- apps/api/__TESTS__/static-info.test.ts
+++ apps/api/__TESTS__/static-info.test.ts
@@ -1,18 +1,19 @@
 import { expect } from 'chai';
 import { mapToStorageLayout } from '../src/static-info';
-import { TEST_CASE_1 } from './test-cases';
+import { TEST_CASE_1, TEST_CASE_2 } from './test-cases';
+
+const toStorageInp = (inp: { disks: any; blocks: any; sizes: any }) =>
+  [inp.disks, inp.blocks, inp.sizes] as const;
 
 describe('Static Info', () => {
   describe('Storage', () => {
     it('Test Case 1', () => {
-      const output = mapToStorageLayout(
-        TEST_CASE_1.disks,
-        //@ts-ignore
-        TEST_CASE_1.blocks,
-        TEST_CASE_1.sizes
-      );
-
+      const output = mapToStorageLayout(...toStorageInp(TEST_CASE_1));
       expect(output).to.deep.equal(TEST_CASE_1.layout);
+    });
+    it('Test Case 2', () => {
+      const output = mapToStorageLayout(...toStorageInp(TEST_CASE_2));
+      expect(output).to.deep.equal(TEST_CASE_2.layout);
     });
   });
 });
