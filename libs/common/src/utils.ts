8204d84133973c2870b08aa2ccb4b3124b08ccbd
MauriceNino <mauriceprivat98@gmail.com>
2022-11-13 17:12:50 +0100

    fix(api): remove broken universal-speedtest to avoid confusion

--- libs/common/src/utils.ts
+++ libs/common/src/utils.ts
@@ -1,3 +1,14 @@
 export const clamp = (num: number, min: number, max: number) => {
   return num > max ? max : num < min ? min : num;
 };
+
+export const removePad = (strings: TemplateStringsArray, ...content: any[]) => {
+  const complete = strings.reduce(
+    (acc, s, i) => `${acc}${s}${content[i] ?? ''}`,
+    ''
+  );
+  const trimmed = complete.split('\n').filter(s => /^\s*$/.test(s) === false);
+  const shortestPad = Math.min(...trimmed.map(s => /(?!\s)/.exec(s)!.index));
+
+  return trimmed.map(s => s.substring(shortestPad)).join('\n');
+};
