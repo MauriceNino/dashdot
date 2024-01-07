3c73ee4f93d9ceb3a5469d580fad4dce22dcda3f
MauriceNino <mauriceprivat98@gmail.com>
2024-01-07 17:00:31 +0100

    feat: add timestamp of last speedtest run on hover
    fixes #915

--- apps/view/src/utils/format.ts
+++ apps/view/src/utils/format.ts
@@ -1,17 +1,22 @@
 export type InfoTableArr = {
   label: string;
   value?: string;
+  tooltip?: string;
 }[];
 
 export const toInfoTable = <T extends Array<string>>(
   order: T,
-  mappings: Record<T[number], { label: string; value?: string | number }>
+  mappings: Record<
+    T[number],
+    { label: string; value?: string | number; tooltip?: string }
+  >
 ): InfoTableArr =>
   order.map(key => {
-    const { label, value } = mappings[key as T[number]];
+    const { label, tooltip, value } = mappings[key as T[number]];
 
     return {
       label,
+      tooltip,
       value: value
         ? typeof value === 'number'
           ? value.toString()
